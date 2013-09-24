ad28accb856016d1aeca98d1cb49f0802a4fcca1
Robert Ross <robert@creativequeries.com>
2013-09-24 11:50:22 -0700

    Initial Commit

---
+++ Huetastic/HUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HUAppDelegate.h
+//  Huetastic
+//
+//  Created by Robert Ross on 9/24/13.
+//  Copyright (c) 2013 Creative Queries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Huetastic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Huetastic
+//
+//  Created by Robert Ross on 9/24/13.
+//  Copyright (c) 2013 Creative Queries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HUAppDelegate class]));
+    }
+}
